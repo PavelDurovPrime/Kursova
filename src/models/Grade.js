3c24062bec6df4d7d4e07631099904af4ba5fe37
Vulture <screamzyaka@gmail.com>
2026-04-09 22:17:20 +0300

    Add domain models Student and Grade classes

---
+++ src/models/Grade.js
@@ -0,0 +1,12 @@
+class Grade {
+  constructor(studentId, subject, value, semester, attendedLessons, totalLessons) {
+    this.studentId = studentId;
+    this.subject = subject;
+    this.value = value;
+    this.semester = semester;
+    this.attendedLessons = attendedLessons;
+    this.totalLessons = totalLessons;
+  }
+}
+
+module.exports = { Grade };
---
+++ src/models/Student.js
@@ -0,0 +1,10 @@
+class Student {
+  constructor(id, fullName, group) {
+    this.id = id;
+    this.fullName = fullName;
+    this.group = group;
+  }
+}
+
+module.exports = { Student };
+
